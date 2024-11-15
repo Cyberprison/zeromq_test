74fdaaaf2c9ae090cf4e370edaffbf08bda82f88
whoami <az.khabibullin2018@gmail.com>
2024-11-15 20:14:01 +0500

    инит проект добавил клиента и сервера nu get pack, zeroMQ NetMQ version 4 hello world example

--- zeromq_test/Program.cs
+++ zeromq_test/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
 
-namespace zeromq_test
+namespace zeromq_test_server
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5556");
+                string msg = server.ReceiveFrameString();
+                Console.WriteLine("From Client: {0}", msg);
+                server.SendFrame("World");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ zeromq_test_client/Program.cs
@@ -0,0 +1,22 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace zeromq_test_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://127.0.0.1:5556");
+                client.SendFrame("Hello");
+                var msg = client.ReceiveFrameString();
+                Console.WriteLine("From Server: {0}", msg);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
