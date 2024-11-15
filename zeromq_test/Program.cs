9d90011675ea45d614d9805918dc4f489f68bf6e
whoami <az.khabibullin2018@gmail.com>
2024-11-15 21:49:09 +0500

    рабочий вариант, если запускать в ms vs 2019 как несколько проектов одновременно
    если запускать как ехе по отдельности, то выбрасывает исключение, тк то клиента нет, то сервера
    
    сейчас попробую в другой ветке попрактиковаться с запуском нового процесса

--- zeromq_test/Program.cs
+++ zeromq_test/Program.cs
@@ -8,15 +8,35 @@ namespace zeromq_test_server
     {
         static void Main(string[] args)
         {
-            using (var server = new ResponseSocket())
+            using (var server = new ResponseSocket("@tcp://localhost:5556"))
             {
-                server.Bind("tcp://*:5556");
                 string msg = server.ReceiveFrameString();
-                Console.WriteLine("From Client: {0}", msg);
-                server.SendFrame("World");
-            }
+                server.SendFrame("Сервер запущен.");
 
-            Console.ReadKey();
+                while (true)
+                {
+                    msg = server.ReceiveFrameString();
+
+                    if (msg == "exit")
+                    {
+                        server.SendFrame("Сервер остановлен.");
+                        break;
+                    }
+                    else
+                    {
+                        //IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "," };
+                        if (double.TryParse(msg, out double number))
+                        {
+                            double result = number * number;
+                            server.SendFrame(result.ToString());
+                        }
+                        else
+                        {
+                            server.SendFrame("Вы ввели не число. Попробуйте снова...");
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- zeromq_test_client/Program.cs
+++ zeromq_test_client/Program.cs
@@ -1,22 +1,78 @@
 ﻿using NetMQ;
 using NetMQ.Sockets;
 using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace zeromq_test_client
 {
     class Program
     {
+        private static Process _childProcess;
+
         static void Main(string[] args)
         {
-            using (var client = new RequestSocket())
+            // Запуск дочернего процесса
+            //StartChildProcess();
+
+            using (var client = new RequestSocket(">tcp://127.0.0.1:5556"))
             {
-                client.Connect("tcp://127.0.0.1:5556");
-                client.SendFrame("Hello");
+                client.SendFrame("Hello from client");
                 var msg = client.ReceiveFrameString();
-                Console.WriteLine("From Server: {0}", msg);
+                Console.WriteLine(msg);
+
+                Console.WriteLine("Связь с сервером установлена.");
+                Console.WriteLine("Для завершения работы введите exit.");
+
+                while (true)
+                {
+                    Console.WriteLine("Введите число");
+                    var input = Console.ReadLine();
+
+                    client.SendFrame(input);
+                    msg = client.ReceiveFrameString();
+                    Console.WriteLine(msg);
+
+                    if (input == "exit")
+                    {
+                        // Завершение дочернего процесса
+                        //StopChildProcess();
+
+                        break;
+                    }
+                }
             }
 
+            Console.WriteLine("Нажмите любую клавишу для завершения работы консольного приложения...");
             Console.ReadKey();
+        }
+
+        private static void StartChildProcess()
+        {
+            _childProcess = new Process();
+
+            // Используем относительный путь
+            _childProcess.StartInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "zeromq_test_server.exe");
+
+            // Без оконного режима
+            //_childProcess.StartInfo.UseShellExecute = false;
+
+            // Не создавать окно
+            //_childProcess.StartInfo.CreateNoWindow = true; 
+            _childProcess.Start();
+
+            Console.WriteLine("Дочерний процесс запущен.");
+        }
+
+        private static void StopChildProcess()
+        {
+            if (_childProcess != null && !_childProcess.HasExited)
+            {
+                // Завершение дочернего процесса
+                _childProcess.Kill(); 
+                _childProcess.Dispose();
+                Console.WriteLine("Дочерний процесс завершен.");
+            }
         }
     }
 }
