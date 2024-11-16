aee9cc5ae19fe805268f2fa157a4901bc58c44a5
whoami <az.khabibullin2018@gmail.com>
2024-11-16 23:04:19 +0500

    финал

--- zeromq_test_client/Program.cs
+++ zeromq_test_client/Program.cs
@@ -3,56 +3,48 @@ using NetMQ.Sockets;
 using System;
 using System.Diagnostics;
 using System.IO;
-
 namespace zeromq_test_client
 {
     class Program
     {
         private static Process _childProcess;
-
         static void Main(string[] args)
         {
-            // Обработка закрытия консоли
             Console.CancelKeyPress += (sender, e) =>
             {
-                e.Cancel = true; // Предотвращаем немедленное завершение
+                e.Cancel = true; 
                 Console.WriteLine("Завершение основного приложения...");
-
-                // Завершение дочернего процесса
                 StopChildProcess();
             };
-
-            // Запуск дочернего процесса
             StartChildProcess();
-
             using (var client = new RequestSocket(">tcp://127.0.0.1:5556"))
             {
-                client.SendFrame("Hello from client");
-                var msg = client.ReceiveFrameString();
-                Console.WriteLine(msg);
-
-                Console.WriteLine("Связь с сервером установлена.");
-                Console.WriteLine("Для завершения работы введите exit.");
-
-                while (true)
+                try
                 {
-                    Console.WriteLine("Введите число");
-                    var input = Console.ReadLine();
-
-                    client.SendFrame(input);
-                    msg = client.ReceiveFrameString();
+                    client.SendFrame("Hello from client");
+                    var msg = client.ReceiveFrameString();
                     Console.WriteLine(msg);
-
-                    if (input == "exit")
+                    Console.WriteLine("Связь с сервером установлена.");
+                    Console.WriteLine("Для завершения работы введите exit.");
+                    while (true)
                     {
-                        // Завершение дочернего процесса
-                        StopChildProcess();
-
-                        break;
+                        Console.WriteLine("Введите число");
+                        var input = Console.ReadLine();
+                        client.SendFrame(input);
+                        msg = client.ReceiveFrameString();
+                        Console.WriteLine(msg);
+                        if (input == "exit")
+                        {
+                            break;
+                        }
                     }
                 }
+                catch(Exception ex) { }
+                finally
+                {
+                    StopChildProcess();
+                }
             }
-
             Console.WriteLine("Нажмите любую клавишу для завершения работы консольного приложения...");
             Console.ReadKey();
         }
@@ -60,17 +52,11 @@ namespace zeromq_test_client
         private static void StartChildProcess()
         {
             _childProcess = new Process();
-
-            // Используем относительный путь
-            _childProcess.StartInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "\\win-x64\\zeromq_test_server");
-
-            // Без оконного режима
-            //_childProcess.StartInfo.UseShellExecute = false;
-
-            // Не создавать окно
-            //_childProcess.StartInfo.CreateNoWindow = true; 
+            _childProcess.StartInfo.FileName = Path
+                .Combine(AppDomain.CurrentDomain.BaseDirectory + "\\win-x64\\zeromq_test_server");
+            _childProcess.StartInfo.UseShellExecute = false;
+            _childProcess.StartInfo.CreateNoWindow = true; 
             _childProcess.Start();
-
             Console.WriteLine("Дочерний процесс запущен.");
         }
 
@@ -78,7 +64,6 @@ namespace zeromq_test_client
         {
             if (_childProcess != null && !_childProcess.HasExited)
             {
-                // Завершение дочернего процесса
                 _childProcess.Kill(); 
                 _childProcess.Dispose();
                 Console.WriteLine("Дочерний процесс завершен.");
--- zeromq_test_server/Program.cs
+++ zeromq_test_server/Program.cs
@@ -1,7 +1,6 @@
 ﻿using NetMQ;
 using NetMQ.Sockets;
 using System;
-
 namespace zeromq_test_server
 {
     class Program
@@ -10,32 +9,35 @@ namespace zeromq_test_server
         {
             using (var server = new ResponseSocket("@tcp://localhost:5556"))
             {
-                string msg = server.ReceiveFrameString();
-                server.SendFrame("Сервер запущен.");
-
-                while (true)
+                try
                 {
-                    msg = server.ReceiveFrameString();
-
-                    if (msg == "exit")
+                    string msg = server.ReceiveFrameString();
+                    server.SendFrame("Сервер запущен.");
+                    while (true)
                     {
-                        server.SendFrame("Сервер остановлен.");
-                        break;
-                    }
-                    else
-                    {
-                        //IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "," };
-                        if (double.TryParse(msg, out double number))
+                        msg = server.ReceiveFrameString();
+                        if (msg == "exit")
                         {
-                            double result = number * number;
-                            server.SendFrame(result.ToString());
+                            server.SendFrame("Сервер остановлен.");
+                            break;
                         }
                         else
                         {
-                            server.SendFrame("Вы ввели не число. Попробуйте снова...");
+                            //IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "," };
+                            if (double.TryParse(msg, out double number))
+                            {
+                                double result = number * number;
+                                server.SendFrame(result.ToString());
+                            }
+                            else
+                            {
+                                server.SendFrame("Вы ввели не число. Попробуйте снова...");
+                            }
                         }
                     }
                 }
+                catch (Exception ex) { }
+                finally { }
             }
         }
     }
